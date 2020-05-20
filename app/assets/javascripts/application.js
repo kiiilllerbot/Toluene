bf98f94ad64771346bc40eaf02b24f72806a18b3
Shimol-Khan <se.shimolkhan@gmail.com>
2020-05-21 00:41:00 +0800

    Solve TinyMCE Issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,59 +25,10 @@ $(document).ready(function (){
 document.addEventListener("turbolinks:load", function() {
 	tinymce.remove();
 	tinymce.init({
-	  height: '220',
-	  selector:'textarea#project_description',
-	  branding: false,
-	  plugins: "codesample image media link code",
-	  toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	});
-
-	tinymce.init({
-		height: '220',
-		selector:'textarea#task_description',
-		branding: false,
-		plugins: "codesample image media link code",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-
-	tinymce.init({
-		height: '220',
-		selector:'textarea#issue_description',
-		branding: false,
-		plugins: "codesample image media link code",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-	
-	tinymce.init({
 		height: '220',
 		selector:'textarea#comment_description',
 		branding: false,
 		plugins: "codesample image media link code",
 		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-
-	  tinymce.init({
-		height: '220',
-		selector:'textarea#note_body',
-		branding: false,
-		plugins: "codesample image media link code",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-
-	  tinymce.init({
-		height: '220',
-		selector:'textarea#work_description',
-		branding: false,
-		plugins: "codesample image media link code",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-
-	tinymce.init({
-		height: '220',
-		selector:'textarea#bug_body',
-		branding: false,
-		plugins: "codesample image media link code",
-		toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
-	  });
-	  
+	  }); 
   })
