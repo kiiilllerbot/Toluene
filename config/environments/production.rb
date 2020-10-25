1501abdc498a3ca30e6e2aff9408cc15fc4bb324
Shimol-Khan <shimolkhan.dev@gmail.com>
2020-10-25 23:29:31 +0800

    Updated Config File

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://shimol-khan-helium.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://shimolkhan-toluene.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   
   ActionMailer::Base.smtp_settings = {
