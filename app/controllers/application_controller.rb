c19018bf434bef70740128da31da711d330cba5a
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-01-02 01:06:54 +0800

    White-List Name Field In Devise Sign Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :password_confirmation,:gender, :title, :bio, :address, :mobile_number, :fb_link, :ig_link, :twitter_link, :github_link, :in_link, :image_link])
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:email])
   end
